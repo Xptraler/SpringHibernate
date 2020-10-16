6eb567f7e2137efe387aa244242085441625b57f
xptraler <xptraler@inbox.lv>
2020-10-16 15:16:41 +0300

    Initial commit1

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,72 @@
+package hiber.model;
+
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "series")
+    private int series;
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id &&
+                series == car.series &&
+                Objects.equals(model, car.model) &&
+                Objects.equals(user, car.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, series, user);
+    }
+}
